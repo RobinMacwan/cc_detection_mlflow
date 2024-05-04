eddc1d77541ec1b56d62ffcad320edc4f877ac3d
RobinMacwany <robin_macwan@yahoo.com>
2024-05-04 13:53:38 -0500

    requirement added

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "cc_detection_mlflow"
+AUTHOR_USER_NAME = "RobinMacwan"
+SRC_REPO = "cc_detection_mlflow"
+AUTHOR_EMAIL = "robin.r.macwan@gmail.com"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for Chest cancer app",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "cc_detection_mlflow"},
+    packages=setuptools.find_packages(where="cc_detection_mlflow")
+)
